c6959eb9d572c3e37a5c17f64848b77e2944157f
rick137-dev <caiulorick137@gmail.com>
2023-02-14 23:03:25 +0100

    Aggiunto import e librerie per JavaFx

--- Test.java
+++ Test.java
@@ -1,5 +1,30 @@
-public class Test {
-    public static void main(String[] args){
+import javafx.application.Application;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import javafx.scene.control.Button;
+import javafx.scene.*;
+
+
+public class Test  extends Application {
+    Button button;
+
+    public static void main(String[] args) {
+
         System.out.println("Hello World");
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+                     stage.setTitle("Yes");
+        button = new Button();
+        button.setText("Touch Me");
+        StackPane layout = new StackPane();
+        layout.getChildren().add(button);
+
+        Scene scene = new Scene(layout, 100, 100);
+        stage.setScene(scene);
+        stage.show();
+
     }
 }
