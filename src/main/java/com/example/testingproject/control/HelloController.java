d1c1c0332ea42957a41605c0a9474e870595afb7
rick137-dev <caiulorick137@gmail.com>
2023-02-15 22:17:04 +0100

    Random work on test branch

--- src/main/java/com/example/testingproject/control/HelloController.java
+++ src/main/java/com/example/testingproject/control/HelloController.java
@@ -1,4 +1,4 @@
-package com.example.testingproject;
+package com.example.testingproject.control;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,5 +4,13 @@ module com.example.testingproject {
 
 
     opens com.example.testingproject to javafx.fxml;
-    exports com.example.testingproject;
+    exports com.example.testingproject.view;
+
+
+
+    opens com.example.testingproject.view to javafx.fxml;
+
+    opens com.example.testingproject.control to javafx.fxml;
+exports com.example.testingproject.control;
+
 }
