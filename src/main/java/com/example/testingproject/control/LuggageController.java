9efe341f622fe36c56337f2258f8889cf3c4fc2d
Daniele Romeo <danieleromeo.ele@gmail.com>
2023-02-18 19:10:16 +0100

    Lavoro parte bagagli

---
+++ src/main/java/com/example/testingproject/control/LuggageController.java
@@ -0,0 +1,28 @@
+package com.example.testingproject.control;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class LuggageController{
+
+    @FXML
+    private Button buttonLuggageDepartures;
+    @FXML
+    private Button buttonLuggageArrivals;
+    @FXML
+    private Button buttonLuggageManage;
+    @FXML
+    private Button buttonLuggageFly;
+
+    @FXML
+    protected void openLuggageDeparturesView() {}
+    @FXML
+    protected void openArrivalsLuggageView() {}
+
+    @FXML
+    protected void openLuggageManageView() {}
+
+    @FXML
+    protected void openLuggageFlyView() {}
+
+}
---
+++ src/main/java/com/example/testingproject/view/luggage/LuggageMainApplication.java
@@ -0,0 +1,24 @@
+package com.example.testingproject.view.luggage;
+
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class LuggageMainApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(LuggageMainApplication.class.getResource("mainLuggageView.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setTitle("Luggage");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
