fe32bda92a793a1599b66e01d30dfddc5c723c9a
rick137-dev <caiulorick137@gmail.com>
2023-02-19 00:56:10 +0100

    Lavoro su funzionamento logico di Login Page

--- src/main/java/com/example/testingproject/control/LoginController.java
+++ src/main/java/com/example/testingproject/control/LoginController.java
@@ -1,12 +1,43 @@
 package com.example.testingproject.control;
 
+import com.example.testingproject.view.login.LoginPage;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+import java.io.IOException;
 
 public class LoginController {
+    public void LogIn(){}
+    @FXML
+private TextField InputUsername;
+    @FXML
+    private PasswordField InputPassword;
 
-private String SampleUsername = "username";
-    private String SamplePassword = "password123";
+    @FXML
+    private Label WrongLogIn;
+
+    @FXML
+    protected void onLogInButtonClick(ActionEvent event) throws IOException {
+    LoginPage page = new LoginPage();
+
+        if(InputUsername.getText().isEmpty() && InputPassword.getText().isEmpty()){
+            WrongLogIn.setText("Please Input Log In Data");
+        }
+
+     else if(InputUsername.toString().equals("admin1") && InputPassword.toString().equals("password")){
+       page.ChangeScene("home_view.fxml");
+     }
+
+     else{
+         WrongLogIn.setText("Wrong Username or Password");
+     }
+    }
 
 
 
--- src/main/java/com/example/testingproject/view/login/LoginPage.java
+++ src/main/java/com/example/testingproject/view/login/LoginPage.java
@@ -2,22 +2,29 @@ package com.example.testingproject.view.login;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class LoginPage extends Application {
+    private static Stage stage;
     @Override
-    public void start(Stage stage) throws IOException {
+    public void start(Stage PrimaryStage) throws IOException {
+        stage = PrimaryStage;
         FXMLLoader fxmlLoader = new FXMLLoader(LoginPage.class.getResource("login_view.fxml"));
-        System.out.print(LoginPage.class.getResource("login_view.fxml"));
+       PrimaryStage.setResizable(false);
       Scene scene = new Scene(fxmlLoader.load(), 1024, 512);
-        stage.setTitle("Login Page");
-        stage.setScene(scene);
-        stage.show();
+        PrimaryStage.setTitle("Login Page");
+        PrimaryStage.setScene(scene);
+        PrimaryStage.show();
     }
 
+    public void ChangeScene(String fxml) throws IOException{
+        Parent pane = FXMLLoader.load(getClass().getResource(fxml));
+        stage.getScene().setRoot(pane);
+    }
     public static void main(String[] args) {
         launch();
     }
