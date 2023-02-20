4956c0a087aa969096e3ef396d531a35f7e95726
rick137-dev <caiulorick137@gmail.com>
2023-02-21 00:23:31 +0100

    Modifiche a funzionamento di LoadingPage, funzionamento come Slpashscreen intanto che si formi la connessione a DB

--- src/main/java/com/example/testingproject/control/LoadingPageController.java
+++ src/main/java/com/example/testingproject/control/LoadingPageController.java
@@ -1,49 +1,97 @@
 package com.example.testingproject.control;
 
+import com.example.testingproject.view.login.LoginPage;
+import javafx.concurrent.Task;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-//import java.math.BigDecimal;
-import java.net.URL;
-import java.util.ResourceBundle;
-//import javafx.beans.value.ChangeListener;
-//import javafx.beans.value.ObservableValue;
-//import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.ProgressBar;
-//import javafx.scene.control.Slider;
 
-public class LoadingPageController implements Initializable{
+import java.io.IOException;
+import java.net.URL;
+
+import java.util.ResourceBundle;
+import java.util.concurrent.TimeUnit;
+
+
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.ProgressBar;
+
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+
+public class LoadingPageController implements Initializable {
+
+    private Stage stage = this.stage;
 
     @FXML
     private ProgressBar myProgressBar;
 
     @FXML
-    private Button myButton;
+    private Pane mainLayout;
 
-    @FXML
-    private Label myLabel;
-
-    //The BigDecimal class gives its user complete control over rounding behavior
-  //  BigDecimal progress = new BigDecimal(String.format("%.2f", 0.0));
-    double progress;
 
     @Override
     public void initialize(URL arg0, ResourceBundle arg1) {
 
         myProgressBar.setStyle("-fx-accent: #00FF00;");
+        Task<Void> task = new Task<Void>() {
+
+            @Override
+            public Void call() throws InterruptedException{
+                try {
+
+                    for(int i=0;i<=4000;i++){
+                        TimeUnit.MILLISECONDS.sleep(1);
+                        myProgressBar.setProgress(i*0.00025);
+
+
+
+
+                    }
+                    TimeUnit.MILLISECONDS.sleep(250);
+
+
+                } catch (Exception e) {
+
+                    System.exit(1);
+                }
+
+                return null;
+            }
+        };
+
+        task.setOnSucceeded(e -> {
+
+
+            FXMLLoader fxmlLoader = new FXMLLoader(LoginPage.class.getResource("login_view.fxml"));
+            Scene scene = null;
+            try {
+
+                scene = new Scene(fxmlLoader.load(), 1024, 512);
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+            Stage stage = new Stage();
+            stage.setResizable(false);
+            stage.setTitle("Login");
+            stage.setScene(scene);
+
+            stage.show();
+            ((Stage) mainLayout.getScene().getWindow()).close();
+        });
+
+
+
+        new Thread(task).start();
+
     }
 
-    public void increaseProgress() {
 
-        if(progress< 0.9999999999999999) {          //per farlo fermare a 100 lo faccio fermare a 0.9999999999999999
-            progress += 0.1;
-            System.out.println(progress);
-            myProgressBar.setProgress(progress);
-            myLabel.setText("            " + Integer.toString((int)Math.round(progress * 100)) + "%");  //gli spazi tra apici sono per permettere che la percentuale stia al centro
-        }
     }
-}
+
+
 
 
 
--- src/main/java/com/example/testingproject/view/LoadingPage/LoadingApplication.java
+++ src/main/java/com/example/testingproject/view/LoadingPage/LoadingApplication.java
@@ -5,17 +5,18 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
+import com.example.testingproject.control.LoadingPageController;
 import java.io.IOException;
 
 public class LoadingApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(LoadingApplication.class.getResource("Loading_Page.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 580, 400);
+        Scene scene = new Scene(fxmlLoader.load(), 1024, 512);
         stage.setTitle("Loading Page");
         stage.setScene(scene);
         stage.show();
+
     }
 
     public static void main(String[] args) {
