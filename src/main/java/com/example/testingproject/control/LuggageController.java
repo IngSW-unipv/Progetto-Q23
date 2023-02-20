f5e8e7dc1dfb3c798f14d4262c605ec1f321a5d1
Daniele Romeo <danieleromeo.ele@gmail.com>
2023-02-20 23:02:14 +0100

    Add luggage manage page

--- src/main/java/com/example/testingproject/control/LuggageController.java
+++ src/main/java/com/example/testingproject/control/LuggageController.java
@@ -1,9 +1,18 @@
 package com.example.testingproject.control;
 
+import com.example.testingproject.view.homePage.HomePage;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
+import javafx.stage.Stage;
+
+import com.example.testingproject.view.homePage.HomePage;
 public class LuggageController{
 
     @FXML
@@ -15,17 +24,28 @@ public class LuggageController{
     @FXML
     private Button buttonLuggageFly;
     @FXML
-    private Label welcomeText;
+    private Label Label;
+
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+    @FXML
+    protected void openLuggageDeparturesView() {Label.setText("Departure");}
+    @FXML
+    protected void openArrivalsLuggageView() {Label.setText("Arrivals");}
 
     @FXML
-    protected void openLuggageDeparturesView() {welcomeText.setText("Departure");}
-    @FXML
-    protected void openArrivalsLuggageView() {welcomeText.setText("Arrivals");}
+    protected void openLuggageManageView() {Label.setText("Manage");
+      /* System.out.print(Manage.class.getResource("manageView.fxml"));
+        root = FXMLLoader.load(ManageMain.class.getResource("manageView.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root,1024, 512);
+        stage.setScene(scene);
+        stage.show();
+*/
+    }
 
     @FXML
-    protected void openLuggageManageView() {welcomeText.setText("Manage");}
-
-    @FXML
-    protected void openLuggageFlyView() {welcomeText.setText("Fly");}
+    protected void openLuggageFlyView() {Label.setText("Fly");}
 
 }
