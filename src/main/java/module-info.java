2f77aec0291f8883d6a1a1acc16c34ce37ff0f50
rick137-dev <caiulorick137@gmail.com>
2023-02-21 19:36:58 +0100

    Fix

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,6 +6,7 @@ module com.example.testingproject {
 
     opens com.example.testingproject to javafx.fxml;
     exports com.example.testingproject.view;
+
     opens com.example.testingproject.view to javafx.fxml;
     exports com.example.testingproject.control;
     opens com.example.testingproject.control to javafx.fxml;
@@ -26,4 +27,5 @@ module com.example.testingproject {
 
 
     exports com.example.testingproject.view.luggage.manage to javafx.graphics;
+
 }
