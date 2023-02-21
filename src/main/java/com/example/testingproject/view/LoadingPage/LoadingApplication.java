503c9949402050a3de4fb8a1180e87315837185b
rick137-dev <caiulorick137@gmail.com>
2023-02-21 18:38:54 +0100

    Fix in LuggageController

--- src/main/java/com/example/testingproject/view/LoadingPage/LoadingApplication.java
+++ src/main/java/com/example/testingproject/view/LoadingPage/LoadingApplication.java
@@ -5,7 +5,6 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import com.example.testingproject.control.LoadingPageController;
 import java.io.IOException;
 
 public class LoadingApplication extends Application {
