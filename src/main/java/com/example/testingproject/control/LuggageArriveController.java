6185c45cc483e71a482bf529a8114c67dcd3b64b
Daniele Romeo <danieleromeo.ele@gmail.com>
2023-02-21 22:07:40 +0100

    Aggiunta vista per gestione bagagli ed arrivo bagagli

--- src/main/java/com/example/testingproject/control/LuggageArriveController.java
+++ src/main/java/com/example/testingproject/control/LuggageArriveController.java
@@ -1,4 +1,29 @@
 package com.example.testingproject.control;
 
+import com.example.testingproject.view.luggage.LuggageMainApplication;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class LuggageArriveController {
+    @FXML
+    private Button searchButton;
+    @FXML
+    private TextField textField;
+    @FXML
+    private ListView listView;
+
+    @FXML
+    private void search(){
+    }
 }
