5dc101478bcd58cbdafbfe0c64d71efb757eb8d1
LibraProtocol <libraprotocol@gmail.com>
2023-02-22 15:54:25 +0100

    update homepage

--- src/main/java/com/example/testingproject/control/HomePageController.java
+++ src/main/java/com/example/testingproject/control/HomePageController.java
@@ -1,8 +1,37 @@
 package com.example.testingproject.control;
 
 
+import com.example.testingproject.view.Voli.VoliArrivo.VoliArrivoApplication;
+import com.example.testingproject.view.login.LoginPage;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class HomePageController{
 
+    @FXML
+    private Button buttonlogin;
+    @FXML
+    private javafx.scene.control.Label Label;
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
 
+    @FXML
+    protected void openLoginView(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(LoginPage.class.getResource("login_view.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root, 1024, 512);
+        stage.setScene(scene);
+        stage.show();
+    }
 
 }
