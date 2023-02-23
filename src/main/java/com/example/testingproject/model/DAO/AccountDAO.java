165537e0cd2fd1008c960ad83e839616f623abae
rick137-dev <caiulorick137@gmail.com>
2023-02-23 03:07:08 +0100

    Created DAO files for account and bagagli

--- src/main/java/com/example/testingproject/model/DAO/AccountDAO.java
+++ src/main/java/com/example/testingproject/model/DAO/AccountDAO.java
@@ -1,4 +1,60 @@
 package com.example.testingproject.model.DAO;
 
+import com.example.testingproject.model.Account;
+import com.example.testingproject.model.DatabaseConnection;
+
+import java.sql.*;
+import java.util.ArrayList;
+
 public class AccountDAO {
+    DatabaseConnection connection = new DatabaseConnection();
+
+
+    public ArrayList<Account> getAccounts() throws SQLException {
+        ArrayList<Account> accounts = new ArrayList<>();
+        Connection conn = connection.getConnection();
+        try{
+            //change tipo to type, change database table layout
+            String query = "SELECT username, password, tipo FROM Utente";
+            PreparedStatement preparedStatement = conn.prepareStatement(query);
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            String tempUsername,tempPassword,tempType;
+
+            while(resultSet.next()){
+                tempUsername = resultSet.getString(1);
+                tempPassword = resultSet.getString(2);
+                tempType = resultSet.getString(3);
+                Account tempAccount = new Account(tempUsername,tempPassword,tempType);
+                accounts.add(tempAccount);
+            }
+
+        }
+
+        catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+connection.closeConnection(conn);
+        return accounts;
+    }
+
+    public boolean createAccount(Account account){
+        Connection conn = connection.getConnection();
+        try{
+            String query = "INSERT INTO Utente (" + account.getUsername() + "," + account.getPassword() + "," + account.getUserType() + ")";
+            PreparedStatement preparedStatement =conn.prepareStatement(query);
+            preparedStatement.executeQuery();
+            return true;
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+
+    }
+
+
+
+
 }
