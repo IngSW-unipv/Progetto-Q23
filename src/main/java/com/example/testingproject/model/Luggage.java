aa8f942c3f6c2aab34e7719a2593f8643a0bbc49
Daniele Romeo <danieleromeo.ele@gmail.com>
2023-02-23 22:07:49 +0100

    aggiunta in model bagaglio

--- src/main/java/com/example/testingproject/model/Luggage.java
+++ src/main/java/com/example/testingproject/model/Luggage.java
@@ -1,4 +1,36 @@
 package com.example.testingproject.model;
 
-public class Luggage {
+public class Luggage{
+    public int Volo,Peso;
+    public String Stato;
+
+    public Luggage(int volo, int peso, String stato) {
+        Volo = volo;
+        Peso = peso;
+        Stato = stato;
+    }
+
+    public int getPeso() {
+        return Peso;
+    }
+
+    public void setPeso(int peso) {
+        Peso = peso;
+    }
+
+    public String getStato() {
+        return Stato;
+    }
+
+    public void setStato(String stato) {
+        Stato = stato;
+    }
+
+    public int getVolo() {
+        return Volo;
+    }
+
+    public void setVolo(int volo) {
+        Volo = volo;
+    }
 }
