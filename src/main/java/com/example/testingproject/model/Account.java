955c938b14a02c9d227a3ffd34d2c91b6928f425
rick137-dev <caiulorick137@gmail.com>
2023-02-23 03:06:46 +0100

    Created Account class with singleton DP

--- src/main/java/com/example/testingproject/model/Account.java
+++ src/main/java/com/example/testingproject/model/Account.java
@@ -1,4 +1,31 @@
 package com.example.testingproject.model;
 
+//singleton per istanza di account
+
+import java.lang.constant.Constable;
+
 public class Account {
+    private String username, password, userType;
+
+
+    public Account(String username, String password, String userType) {
+        this.username = username;
+        this.password = password;
+        this.userType = userType;
+    }
+
+    public String getUserType() {
+        return this.userType;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
 }
+
+
+
