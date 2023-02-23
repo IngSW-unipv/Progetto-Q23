d975520e887980eaba0729bbb0d5b624e836ebe7
rick137-dev <caiulorick137@gmail.com>
2023-02-23 03:07:32 +0100

    Fix in DatabaseConnection

--- src/main/java/com/example/testingproject/model/DatabaseConnection.java
+++ src/main/java/com/example/testingproject/model/DatabaseConnection.java
@@ -2,23 +2,41 @@ package com.example.testingproject.model;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class DatabaseConnection {
 
-    public Connection DatabaseLink;
+    public Connection connection;
+   private String databaseUser = "root";
+   private String databasePassword = "riccardo12caiulo";
+   private String url = "jdbc:mysql://34.68.181.102:3306/%s";
+
 
     public Connection getConnection(){
-        String databaseName ="java";
-        String databaseUser = "root";
-        String databasePassword = "riccardo12caiulo";
-        String url = "34.68.181.102" +databaseName;
+
         try{
             Class.forName("com.mysql.cj.jdbc.Driver");
-            DatabaseLink = DriverManager.getConnection(url,databaseUser,databasePassword);
+            connection = DriverManager.getConnection(url,databaseUser,databasePassword);
         }catch(Exception exception){
             exception.printStackTrace();
         }
-        return DatabaseLink;
+        return connection;
     }
+
+
+    public void closeConnection(Connection connection) throws SQLException {
+
+       if(connection != null){
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+
+
 
 }
