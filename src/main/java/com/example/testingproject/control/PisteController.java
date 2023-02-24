ecd7e5cf1db5a227fbce7667dc16d50411cacf2e
rick137-dev <caiulorick137@gmail.com>
2023-02-24 18:54:17 +0100

    Added Pista Application, Controller and FXML file

---
+++ src/main/java/com/example/testingproject/control/PisteController.java
@@ -0,0 +1,11 @@
+package com.example.testingproject.control;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class PisteController {
+    @FXML
+    private Label welcomeText;
+
+
+}
--- src/main/java/com/example/testingproject/model/Pista.java
+++ src/main/java/com/example/testingproject/model/Pista.java
@@ -1,7 +1,28 @@
 package com.example.testingproject.model;
 
 public class Pista {
+    private String direction;
+    private int length, id;
 
+    public Pista(int id, int length, String direction){
+        this.id = id;
+        this.length = length;
+        this.direction = direction;
+    }
+
+
+    public int getId(){
+        return this.id;
+    }
+
+    public int getLength(){
+        return this.length;
+
+    }
+
+    public String getDirection(){
+        return this.direction;
+    }
 
 
 
