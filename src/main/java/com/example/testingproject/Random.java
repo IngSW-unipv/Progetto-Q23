df88edcb8dcb446d8d72644ef99408bb68e4416d
rick137-dev <caiulorick137@gmail.com>
2023-02-25 20:50:25 +0100

    Added functionality for Piste page

--- src/main/java/com/example/testingproject/Random.java
+++ src/main/java/com/example/testingproject/Random.java
@@ -3,6 +3,7 @@ import com.example.testingproject.model.Account;
 
 import com.example.testingproject.model.DAO.AccountDAO;
 import com.example.testingproject.model.Pista;
+import com.example.testingproject.model.Wind;
 import com.example.testingproject.model.service.AccountService;
 import com.example.testingproject.model.service.PistaService;
 
@@ -14,8 +15,11 @@ import java.util.ArrayList;
 public class Random {
 
     public static void main(String[] args) throws SQLException {
+        Wind wind = new Wind();
         System.out.println("Testing class\n");
-
+for(int i =0;i<10;i++){
+    System.out.print("\n"+  wind.getDirection());
+}
 
 
 
