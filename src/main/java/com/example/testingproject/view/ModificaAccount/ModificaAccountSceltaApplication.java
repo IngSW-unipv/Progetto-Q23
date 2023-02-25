f0e106a50779f422139c136d56770b3471e5bf2d
RubenRcp <ruben.canevari01@universitadipavia.it>
2023-02-25 18:55:46 +0100

    Creazione classe ModificaAccountSceltaApplication

---
+++ src/main/java/com/example/testingproject/view/ModificaAccount/ModificaAccountSceltaApplication.java
@@ -0,0 +1,26 @@
+package com.example.testingproject.view.ModificaAccount.;
+
+
+        import javafx.application.Application;
+        import javafx.fxml.FXMLLoader;
+        import javafx.scene.Scene;
+        import javafx.stage.Stage;
+
+
+        import java.io.IOException;
+
+public class ModificaAccountSceltaApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {             //Errore da rivedere
+        FXMLLoader fxmlLoader = new FXMLLoader(com.example.testingproject.view.ModificaAccountSceltaApplication.class.getResource("ModificaAccount.fxml"));
+        System.out.print(com.example.testingproject.view.ModificaAccountSceltaApplication.class.getResource("ModificaAccount.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setTitle("Scelta della modifica");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
