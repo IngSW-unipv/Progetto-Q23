a5d36f1e3e850d562ed8c92c12b609a107716a47
rick137-dev <caiulorick137@gmail.com>
2023-02-28 18:55:23 +0100

    Added 3 J Unit tests for accountDAO and accountService

--- src/main/java/Test/MethodsTests.java
+++ src/main/java/Test/MethodsTests.java
@@ -1,15 +1,46 @@
 package Test;
 
+import com.example.testingproject.model.Account;
+import com.example.testingproject.model.DAO.AccountDAO;
+import com.example.testingproject.model.service.AccountService;
 import org.testng.annotations.Test;
 
+import java.sql.SQLException;
+
+import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
 
 public class MethodsTests {
+    AccountService accountService = new AccountService();
+    AccountDAO accountDAO = new AccountDAO();
+
+
 
     @Test
-    void findAccountbyUsernameTest(){
-      assertTrue(true);
+    void findAccountbyUsernameDAOTest() throws SQLException {
+        String username = "daniele";
+        Account tempaccount = accountDAO.getAccountbyUsername(username);
+        assertEquals(tempaccount.getUsername(),username);
+
+
     }
 
+    @Test
+    void findAccountbyUsernameServiceTest() throws SQLException {
+        String username = "daniele";
+        Account tempaccount = accountService.findAccountByUsername(username);
+        assertEquals(tempaccount.getUsername(),username);
+
+
+    }
+
+    @Test
+    void addAccountDAOTest() throws SQLException {
+        Account account = new Account("check","check","Voli","chek","check");
+        if(accountService.findAccountByUsername(account.getUsername())==null){ //per vedere se account con quel username esiste già
+            accountDAO.createAccount(account);
+            assertEquals(accountService.findAccountByUsername(account.getUsername()).getUsername(),account.getUsername());
+        }
+    }
 
 }
