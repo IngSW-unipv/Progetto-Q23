790c530ac8e59a9435ae67a2f2e2e5f892ea8246
rick137-dev <caiulorick137@gmail.com>
2023-02-28 18:33:47 +0100

    Added functionality for Account Handling, also fixes

--- src/main/java/com/example/testingproject/control/GestioneAccountController.java
+++ src/main/java/com/example/testingproject/control/GestioneAccountController.java
@@ -48,4 +48,14 @@ public class GestioneAccountController {
         stage.show();
 
     }
+
+
+    public void goToHome(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(HomePage.class.getResource("homePage_view.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root, 1024, 512);
+        stage.setScene(scene);
+        stage.show();
+    }
+
 }
